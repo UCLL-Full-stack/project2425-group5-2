eba34eb85ef4c7a9f657606502208ec518ee7f3d
Sander Coemans <sander.coemans@student.ucll.be>
2024-12-11 15:50:18 +0100

    login page

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import Layout from '../components/layout/Layout';
 
 const Home: React.FC = () => {
---
+++ front-end/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import Login from '../../components/auth/Login';
+
+const LoginPage = () => {
+    return (
+      <>
+        <title>Login</title>
+        <Login />
+      </>
+    );
+  };
+  
+  export default LoginPage;
