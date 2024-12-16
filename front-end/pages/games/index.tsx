b0c1c8419c55930ed358889a5809fec1198aa287
Rajo <rajo.timmermans@gmail.com>
2024-12-16 12:25:25 +0100

    gameoverview in the making

--- front-end/pages/games/index.tsx
+++ front-end/pages/games/index.tsx
@@ -8,7 +8,7 @@ import { Plus } from 'lucide-react';
 import GameService from '@services/GameService';
 import GamesOverview from '@components/games/GamesOverview';
 
-const Games: React.FC = () => {
+const GamesPage: React.FC = () => {
     const [teams, setTeams] = useState<Array<Team>>([]);
     const router = useRouter();
     const [loggedInUser, setLoggedInUser] = useState<User>(null);
@@ -24,7 +24,7 @@ const Games: React.FC = () => {
 
     useEffect(() => {
         if (loggedInUser) {
-            getGames();
+            getTeamsAndGames();
         }
     }, [loggedInUser]);
 
@@ -54,48 +54,46 @@ const Games: React.FC = () => {
         if (!loggedInUser) {
             return <p>Loading...</p>;
         }
+    };
 
-        const createGameRoute = () => {
-            router.push('/game/create');
-        };
+    const createGameRoute = () => {
+        router.push('/game/create');
+    };
 
-        return (
-            <Layout>
-                <Head>
-                    <title>Games - TeamTrack</title>
-                </Head>
-                <div className="container mx-auto px-4 py-8">
-                    <div className="bg-gradient-to-br from-primary to-accent p-8 rounded-lg shadow-xl max-w-5xl mx-auto">
-                        <div className="flex justify-between items-center mb-8">
-                            <h1 className="text-4xl font-extrabold text-white tracking-tight">
-                                Games Overview
-                            </h1>
-                            {(loggedInUser.role == 'admin' || loggedInUser.role == 'coach') && (
-                                <button
-                                    onClick={createGameRoute}
-                                    className="px-6 py-3 bg-secondary text-white text-lg font-semibold rounded-md transition-all duration-300 hover:bg-accent hover:shadow-lg transform hover:scale-105 flex items-center"
-                                >
-                                    <Plus size={24} className="mr-2" />
-                                    Create Game
-                                </button>
-                            )}
-                        </div>
-                        {teams.length > 0 ? (
-                            <GamesOverview teams={teams} games={games} />
-                        ) : (
-                            <div className="text-center py-12">
-                                <p className="text-2xl font-semibold text-white mb-4">
-                                    No Games found
-                                </p>
-                                <p className="text-lg text-white">
-                                    Click the 'Create Game' button to create a game!
-                                </p>
-                            </div>
+    return (
+        <Layout>
+            <Head>
+                <title>Games - TeamTrack</title>
+            </Head>
+            <div className="container mx-auto px-4 py-8">
+                <div className="bg-gradient-to-br from-primary to-accent p-8 rounded-lg shadow-xl max-w-5xl mx-auto">
+                    <div className="flex justify-between items-center mb-8">
+                        <h1 className="text-4xl font-extrabold text-white tracking-tight">
+                            Games Overview
+                        </h1>
+                        {(loggedInUser.role == 'admin' || loggedInUser.role == 'coach') && (
+                            <button
+                                onClick={createGameRoute}
+                                className="px-6 py-3 bg-secondary text-white text-lg font-semibold rounded-md transition-all duration-300 hover:bg-accent hover:shadow-lg transform hover:scale-105 flex items-center"
+                            >
+                                <Plus size={24} className="mr-2" />
+                                Create Game
+                            </button>
                         )}
                     </div>
+                    {teams.length > 0 ? (
+                        <GamesOverview teams={teams} games={games} />
+                    ) : (
+                        <div className="text-center py-12">
+                            <p className="text-2xl font-semibold text-white mb-4">No Games found</p>
+                            <p className="text-lg text-white">
+                                Click the 'Create Game' button to create a game!
+                            </p>
+                        </div>
+                    )}
                 </div>
-            </Layout>
-        );
-    };
+            </div>
+        </Layout>
+    );
 };
-export default Games;
+export default GamesPage;
